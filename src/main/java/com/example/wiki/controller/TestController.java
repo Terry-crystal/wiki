dd3a29df2426c5876f38cb814ac2a799be372401
Crystal <1348834664@qq.com>
2021-09-13 21:49:33 +0800

    2-8 增加了自定义配置项示例，优先读取配置项中的值，然后再使用默认值

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController //这个注解一般返回一个字符串 如果使用Controller注解，一般返回一个页面
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     /**
      * GET,POST,PUT,DELETE 四种增删改查请求
      * 使用RequestMapping默认表示这个接口支持所有的请求方式
@@ -20,7 +24,7 @@ public class TestController {
      */
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
