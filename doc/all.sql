e3d200317ac8b5db725e2a1696ba5678decfc96b
Crystal <1348834664@qq.com>
2021-09-14 20:50:15 +0800

    3-5 增加了Demo表列表的查询

--- doc/all.sql
+++ doc/all.sql
@@ -1,20 +1,26 @@
 drop table if exists `test`;
 
-create table `test` (
-  `id` bigint not null comment 'id',
-  `name` varchar(50) comment '名称',
-  `password` varchar(50) comment '密码',
-  primary key (`id`)
-) engine =innodb default charset = utf8mb4
-  comment ='测试'
+create table `test`
+(
+    `id`       bigint not null comment 'id',
+    `name`     varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4
+    comment ='测试'
 
-insert into `test` (id, name, password) VALUES (1,'测试','passwoed');
+insert into `test` (id, name, password)
+VALUES (1, '测试', 'passwoed');
 
 drop table if exists `demo`;
-create table `demo` (
-    `id` bigint not null comment 'id',
+create table `demo`
+(
+    `id`   bigint not null comment 'id',
     `name` varchar(50) comment '名称',
     primary key (`id`)
-)engine =innodb default charset = utf8mb4 comment ='测试'
+) engine = innodb
+  default charset = utf8mb4 comment ='测试'
 
-insert into `demo`(id, name) VALUES (1,'demo表测试');
+insert into `demo`(id, name)
+VALUES (1, 'demo表测试');
--- src/main/java/com/example/wiki/mapper/DemoMapper.java
+++ src/main/java/com/example/wiki/mapper/DemoMapper.java
@@ -2,8 +2,9 @@ package com.example.wiki.mapper;
 
 import com.example.wiki.domain.Demo;
 import com.example.wiki.domain.DemoExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface DemoMapper {
     long countByExample(DemoExample example);
