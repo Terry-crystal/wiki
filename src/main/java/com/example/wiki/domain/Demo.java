923ce2407079817120215f9ff4f1b63a933717a3
Crystal <1348834664@qq.com>
2021-09-14 20:32:36 +0800

    3-5 集成mybatis generator，生成demo表代码

--- src/main/java/com/example/wiki/domain/Demo.java
+++ src/main/java/com/example/wiki/domain/Demo.java
@@ -1,11 +1,9 @@
 package com.example.wiki.domain;
 
-public class Test {
+public class Demo {
     private Long id;
 
     private String name;
-
-    private String password;
 
     public Long getId() {
         return id;
@@ -23,14 +21,6 @@ public class Test {
         this.name = name;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -39,7 +29,6 @@ public class Test {
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
         sb.append(", name=").append(name);
-        sb.append(", password=").append(password);
         sb.append("]");
         return sb.toString();
     }
--- src/main/java/com/example/wiki/mapper/TestMapper.java
+++ src/main/java/com/example/wiki/mapper/TestMapper.java
@@ -4,13 +4,6 @@ import com.example.wiki.domain.Test;
 
 import java.util.List;
 
-/**
- * @author Crystal
- * @version 1.0
- * @date 2021/9/14 19:32
- */
 public interface TestMapper {
-
     public List<Test> list();
-
 }
