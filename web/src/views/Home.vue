37fe80ccb7aad016c5d82d8abb76aa77a8a56295
Crystal <1348834664@qq.com>
2021-09-15 20:58:15 +0800

    5-4 加入Ant Design Vue list组件用于显示初始数据

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -43,28 +43,75 @@
                 </a-sub-menu>
             </a-menu>
         </a-layout-sider>
+
+        <!--中间显示控件-->
         <a-layout-content
                 :style="{ background: '#FFF', padding: '24px', margin: 0, minHeight: '280px' }"
         >
 
-            <pre>{{ebooks}}
-                {{books2}}
-            </pre>
+            <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+                <template #footer>
+                    <div>
+                        <b>ant design vue</b>
+                        footer part
+                    </div>
+                </template>
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.title">
+                        <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+                        </template>
+                        <template #extra>
+                            <img
+                                    width="272"
+                                    alt="logo"
+                                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                            />
+                        </template>
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.title }}</a>
+                            </template>
+                            <template #avatar>
+                                <a-avatar :src="item.avatar"/>
+                            </template>
+                        </a-list-item-meta>
+                        {{ item.content }}
+                    </a-list-item>
+                </template>
+            </a-list>
 
         </a-layout-content>
     </a-layout>
 </template>
 
 <script lang="ts">
-    import {defineComponent, onMounted, reactive, ref,toRef} from 'vue';
+    import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
     import axios from 'axios';
+
+    const listData: Record<string, string>[] = [];
+
+    for (let i = 0; i < 23; i++) {
+        listData.push({
+            href: 'https://www.antdv.com/',
+            title: `ant design vue part ${i}`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        });
+    }
 
     export default defineComponent({
         name: 'Home',
         setup() {
             console.log("setup");
             const ebooks = ref();   //这个是一个响应式的数据！
-            const ebooks1 = reactive({books:[]});   //reactive()里面一般放一个对象
+            const ebooks1 = reactive({books: []});   //reactive()里面一般放一个对象
 
 
             //生命周期初始化方法
@@ -74,12 +121,30 @@
                     console.log(response);
                     const data = response.data; //此data就是后端中的commonResp
                     ebooks.value = data.content;
-                    ebooks1.books=data.content;
+                    ebooks1.books = data.content;
                 });
             });
+
+
+            const pagination = {
+                onChange: (page: number) => {
+                    console.log(page);
+                },
+                pageSize: 3,
+            };
+            const actions: Record<string, string>[] = [
+                {type: 'StarOutlined', text: '156'},
+                {type: 'LikeOutlined', text: '156'},
+                {type: 'MessageOutlined', text: '2'},
+            ];
+
             return {
                 ebooks,
-                books2:toRef(ebooks1,"books")
+                books2: toRef(ebooks1, "books"),
+
+                listData,
+                pagination,
+                actions,
             } //html代码要拿到响应式变量，需要在setup最后return
         }
     });
