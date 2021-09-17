49fc53c5ab6b69d0212e7a19b10a2e3d92d030b9
Crystal <1348834664@qq.com>
2021-09-17 10:41:21 +0800

    6-11 集成Validation，为列表接口增加参数校验

--- src/main/java/com/example/wiki/controller/EbookController.java
+++ src/main/java/com/example/wiki/controller/EbookController.java
@@ -9,6 +9,7 @@ import com.example.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 /**
  * @author Crystal
@@ -29,7 +30,7 @@ public class EbookController {
      * @return 返回对应的电子书列表和对应的分页数据
      */
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq req) {
+    public CommonResp list(@Valid EbookQueryReq req) {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> list = ebookService.list(req);
         resp.setContent(list);
