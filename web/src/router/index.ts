218b8d8ce5e9785cb86e262d280e7b3b2e063925
Crystal <1348834664@qq.com>
2021-09-25 13:49:06 +0800

    8-12 增加文档页面，点击首页电子书可以进入文档页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -4,6 +4,7 @@ import About from '../views/about.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
+import Doc from '../views/doc.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -12,13 +13,14 @@ const routes: Array<RouteRecordRaw> = [
         component: Home
     },
     {
+        path: '/doc',
+        name: 'Doc',
+        component:Doc
+    },
+    {
         path: '/about',
         name: 'About',
         component: About
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
     },
     {
         path: '/admin/ebook',
