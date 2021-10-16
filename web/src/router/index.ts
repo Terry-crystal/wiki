bf4a7143d2a660e49872d13f2ca705698870696f
Crystal <1348834664@qq.com>
2021-10-17 00:30:07 +0800

    9-12 增加路由登录拦截，未登录时跳到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,6 +6,8 @@ import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
 import AdminUser from '../views/admin/admin-user.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -26,27 +28,58 @@ const routes: Array<RouteRecordRaw> = [
     {
         path: '/admin/ebook',
         name: 'AdminEbook',
-        component: AdminEbook
+        component: AdminEbook,
+        meta:{
+            loginRequire:true   //这些是为了防止没有登录的时候直接通过路由访问页面，在这做个参数方便后面拦截
+        }
     },
     {
         path: '/admin/category',
         name: 'AdminCategory',
-        component: AdminCategory
+        component: AdminCategory,
+        meta:{
+            loginRequire:true
+        }
     }, {
         path: '/admin/doc',
         name: 'Admindoc',
-        component: AdminDoc
+        component: AdminDoc,
+        meta:{
+            loginRequire:true
+        }
     },
     {
         path: '/admin/user',
         name: 'AdminUser',
-        component: AdminUser
+        component: AdminUser,
+        meta:{
+            loginRequire:true
+        }
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
+});
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+    // 要不要对meta.loginRequire属性做监控拦截
+    if (to.matched.some(function (item) {
+        console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+        return item.meta.loginRequire
+    })) {
+        const loginUser = store.state.user;
+        if (Tool.isEmpty(loginUser)) {
+            console.log("用户未登录！");
+            next('/');
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 });
 
 export default router
